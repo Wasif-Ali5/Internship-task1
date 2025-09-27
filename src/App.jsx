e922bd98dea8d851c57afb277fce784f46d1971e
wasif raza <wasif.raazaa@gmail.com>
2025-09-27 02:16:15 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+ 
+  return (
+  <div className="h-screen bg-orange-300 flex justify-center items-center">
+    
+    <h1 className="text-4xl md:text-3xl font-bold font-serif tracking-wide">Welcome to the Student Management System</h1>
+
+  </div>
+  )
+}
+
+export default App
